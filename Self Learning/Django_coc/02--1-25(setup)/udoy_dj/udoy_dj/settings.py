6fbb744377d1852621652e8ca0cb1f8ece3332ac
griffin <githublinkedin@gmail.com>
2025-01-07 15:03:06 +0600

    07-01-25

--- Self Learning/Django_coc/02--1-25(setup)/udoy_dj/udoy_dj/settings.py
+++ Self Learning/Django_coc/02--1-25(setup)/udoy_dj/udoy_dj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+# joining static dir with the root directory
+# first import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Self Learning/Django_coc/02--1-25(setup)/udoy_dj/udoy_dj/views.py
+++ Self Learning/Django_coc/02--1-25(setup)/udoy_dj/udoy_dj/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
+
 # from http you are taking http response
 from django.shortcuts import render
+
 
 def home(request):
     # return HttpResponse("hello world , you are at chai or Django homepage ")
     return render(request, "index.html")
+
+
 def about(request):
-    return HttpResponse("hello world , you are at chai or Django About ")
+    # return HttpResponse("hello world , you are at chai or Django About ")
+    return render(request, "about.html")
+
+
 def contact(request):
-    return HttpResponse("hello world , you are at chai or Django contact ")
+    # return HttpResponse("hello world , you are at chai or Django contact ")
+    return render(request, "contact.html")
